5add3e5dd743ea27edeae3d2feb5a3971f21311d
Daniel Rojas <danni3231@hotmail.com>
2021-06-04 15:38:04 -0500

    Profile data
    connected data page to profile

---
+++ scripts/data.js
@@ -0,0 +1,5 @@
+const back = document.querySelector('.misDatosIcons--backIcon');
+
+back.addEventListener('click', ()=>{
+    window.history.go(-1);
+});
---
+++ scripts/profile.js
@@ -0,0 +1,8 @@
+const btnData = document.querySelector('.profileMain__btnData');
+const btnRecord = document.querySelector('.profileMain__btnRecord');
+
+btnData.addEventListener('click', ()=>{
+    location.href=  'misDatos.html';
+});
+
+console.log(btnData);
